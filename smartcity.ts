043e1c4f2df58e65ca0cc93abcd7639b5c7ce5a0
sam.lee <sam.lee@smarthon.cc>
2022-08-09 21:49:03 +0800

    DHT11 code update to auto adjust for V1 and V2 microbit

--- smartcity.ts
+++ smartcity.ts
@@ -3,60 +3,64 @@ namespace SmartCity {
         //% block="off"
         off = 0,
         //% block="on"
-        on =1
+        on = 1
     }
-	export enum DHT11dataType {
-    //% block="temperature"
-    temperature,
-	//% block="humidity"
-    humidity
-	}
-	
-	export enum DistanceUnit {
-    //% block="cm"
-    Centimeters,
-    //% block="inches"
-    Inches,
-	 //% block="μs"
-    MicroSeconds
-	}
-	
-    
+
+
+
+    export enum DHT11dataType {
+        //% block="temperature"
+        temperature,
+        //% block="humidity"
+        humidity
+    }
+
+    export enum DistanceUnit {
+        //% block="cm"
+        Centimeters,
+        //% block="inches"
+        Inches,
+        //% block="μs"
+        MicroSeconds
+    }
+
+
 
     let temp = 0
-	let temp_pin=0
-	let _temperature: number = -999.0
+    let temp_pin = 0
+    let _temperature: number = -999.0
     let _humidity: number = -999.0
     let _readSuccessful: boolean = false
-    let _firsttime:boolean=true
-    let _last_successful_query_temperature: number= 0
+
+    let _firsttime: boolean = true
+    let _last_successful_query_temperature: number = 0
     let _last_successful_query_humidity: number = 0
-	/**
+    /**
      * Set traffic light
      * @param out_red describe parameter here, eg: boolean.true
      * @param out_yellow describe parameter here, eg: boolean.true
      * @param out_green describe parameter here, eg: boolean.true
-     */ 
+     */
 
-	//%subcategory=Output
+    //%subcategory=Output
     //% blockId=control_traffic_light
     //% block="Control traffic light |Red $out_red Yellow $out_yellow Green $out_green at Pin %traffic_pin"
     //% out_red.shadow="toggleOnOff" 
     //% out_yellow.shadow="toggleOnOff" 
     //% out_green.shadow="toggleOnOff"
     //% weight=250
-	
-    export function control_traffic_light( out_red: boolean, out_yellow: boolean, out_green: boolean,traffic_pin: AnalogPin): void {
-        let red=out_red?1:0;
-        let yellow=out_yellow?1:0;
-        let green=out_green?1:0;
-        temp =  2*2*red + 2*yellow +green
-        temp = temp*125
+
+    export function control_traffic_light(out_red: boolean, out_yellow: boolean, out_green: boolean, traffic_pin: AnalogPin): void {
+        let red = out_red ? 1 : 0;
+        let yellow = out_yellow ? 1 : 0;
+        let green = out_green ? 1 : 0;
+        temp = 2 * 2 * red + 2 * yellow + green
+        temp = temp * 125
         pins.analogWritePin(traffic_pin, temp)
-		basic.pause(500)
+        basic.pause(500)
     }
-	
-	//%subcategory=Output
+
+    //%subcategory=Output
     //%blockId=control_Servo
     //%block="Turn Servo to %deg degree |at %pin"
     //% weight=240
@@ -65,8 +69,8 @@ namespace SmartCity {
         pins.servoWritePin(pin, deg)
         basic.pause(500)
     }
-	
-	//%subcategory=Output
+
+    //%subcategory=Output
     //%blockId=control_LED
     //%block="Turn White LED to %intensity |at %pin"
     //% weight=245
@@ -75,58 +79,58 @@ namespace SmartCity {
         pins.analogWritePin(pin, intensity)
         basic.pause(500)
     }
-	
-	//% blockId=read_light_sensor
+
+    //% blockId=read_light_sensor
     //% block="Get light value (percentage) at Pin %light_pin"
     //% weight=225
     export function read_light_sensor(light_pin: AnalogPin): number {
         temp_pin = parseInt(light_pin.toString())
-		temp = Math.round(pins.analogReadPin(temp_pin)/1023*100)
+        temp = Math.round(pins.analogReadPin(temp_pin) / 1023 * 100)
         return temp
     }
 
-	
-	//% blockId=read_raindrop_sensor
+
+    //% blockId=read_raindrop_sensor
     //% block="Get raindrop value (percentage) at Pin %rain_pin"
     //% weight=200
     export function read_raindrop_sensor(rain_pin: AnalogPin): number {
         temp_pin = parseInt(rain_pin.toString())
-		temp = Math.round(pins.analogReadPin(temp_pin)/1023*100)
+        temp = Math.round(pins.analogReadPin(temp_pin) / 1023 * 100)
         return temp
     }
 
-	//% blockId=read_motion_sensor
+    //% blockId=read_motion_sensor
     //% block="Get motion (triggered or not) at Pin %motion_pin"
     //% weight=180
     export function read_motion_sensor(motion_pin: AnalogPin): boolean {
         temp_pin = parseInt(motion_pin.toString())
-		temp = pins.analogReadPin(temp_pin)
-		if (temp > 800)
-			return true
-		else return false
+        temp = pins.analogReadPin(temp_pin)
+        if (temp > 800)
+            return true
+        else return false
     }
-	
-	//% blockId=read_sound_sensors
+
+    //% blockId=read_sound_sensors
     //% block="Get noise level (dB) at Pin %sound_pin"
     //% weight=170
     export function read_sound_sensor(sound_pin: AnalogPin): number {
         temp_pin = parseInt(sound_pin.toString())
-		let sum=0;
-        for(let i=0;i<20;i++){
-		temp = pins.map(Math.abs(pins.analogReadPin(temp_pin) - 520),0,520,0,1023);
-		temp =temp/1023*100
-        sum=sum+temp;
+        let sum = 0;
+        for (let i = 0; i < 20; i++) {
+            temp = pins.map(Math.abs(pins.analogReadPin(temp_pin) - 520), 0, 520, 0, 1023);
+            temp = temp / 1023 * 100
+            sum = sum + temp;
         }
-        return parseInt((sum/20).toString()) ;
+        return parseInt((sum / 20).toString());
     }
-	
+
 
 
     //% block="Get DHT11 at pin %dataPin|"
-    function dht11_queryData( dataPin: DigitalPin) {
+    function dht11_queryData(dataPin: DigitalPin) {
 
-        if(_firsttime==true){
-            _firsttime=false
+        if (_firsttime == true) {
+            _firsttime = false
             dht11_queryData(dataPin)
         }
         //initialize
@@ -142,6 +146,8 @@ namespace SmartCity {
         _temperature = 0
         _readSuccessful = false
 
+
+
         //request data
         pins.digitalWritePin(dataPin, 0) //begin protocol
         basic.pause(18)
@@ -152,83 +158,108 @@ namespace SmartCity {
             //if no respone,exit the loop to avoid Infinity loop
             pins.setPull(dataPin, PinPullMode.PullNone) //release pull up
         }
-        else{
-        pins.setPull(dataPin, PinPullMode.PullNone) //release pull up
-        while (pins.digitalReadPin(dataPin) == 0); //sensor response
-        while (pins.digitalReadPin(dataPin) == 1); //sensor response
+        else {
+            pins.setPull(dataPin, PinPullMode.PullNone) //release pull up
+            while (pins.digitalReadPin(dataPin) == 0); //sensor response
+            while (pins.digitalReadPin(dataPin) == 1); //sensor response
+            //---------------V2--------------------
+            //read data (5 bytes)
+            if (control.ramSize() > 20000) {
+                for (let index = 0; index < 40; index++) {
+                    startTime = input.runningTimeMicros();
+                    while (pins.digitalReadPin(dataPin) == 1) {
+                        endTime = input.runningTimeMicros()
+                        if ((endTime - startTime) > 150) { break; }
+                    };
+                    while (pins.digitalReadPin(dataPin) == 0) {
+                        endTime = input.runningTimeMicros()
+                        if ((endTime - startTime) > 150) { break; }
+                    };
+                    control.waitMicros(28)
+                    //if sensor pull up data pin for more than 28 us it means 1, otherwise 0
+                    if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
+                }
+            }
+			/*----------------V1------------*/
+            else if (control.ramSize() < 20000) {
+                for (let index = 0; index < 40; index++) {
+                    while (pins.digitalReadPin(dataPin) == 1);
+                    while (pins.digitalReadPin(dataPin) == 0);
+                    control.waitMicros(28)
+                    //if sensor still pull up data pin after 28 us it means 1, otherwise 0
+                    if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
+                }
+            }
 
-        //read data (5 bytes)
-        for (let index = 0; index < 40; index++) {
-            startTime = input.runningTimeMicros();
-            while (pins.digitalReadPin(dataPin) == 1){
-                endTime = input.runningTimeMicros()
-                if ((endTime - startTime) > 150) { break; }
-            };
-            while (pins.digitalReadPin(dataPin) == 0){
-                endTime = input.runningTimeMicros()
-                if ((endTime - startTime) > 150) { break; }
-            };
-            control.waitMicros(28)
-            //if sensor pull up data pin for more than 28 us it means 1, otherwise 0
-            if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
-        }
-		
-        //convert byte number array to integer
-        for (let index = 0; index < 5; index++)
-            for (let index2 = 0; index2 < 8; index2++)
-                if (dataArray[8 * index + index2]) resultArray[index] += 2 ** (7 - index2)
 
-        //verify checksum
-        checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
-        checksum = resultArray[4]
-        if (checksumTmp >= 512) checksumTmp -= 512
-        if (checksumTmp >= 256) checksumTmp -= 256
-        if (checksum == checksumTmp) _readSuccessful = true
 
-        //read data if checksum ok
-        if (_readSuccessful) {
+
+
+            //convert byte number array to integer
+            for (let index = 0; index < 5; index++)
+                for (let index2 = 0; index2 < 8; index2++)
+                    if (dataArray[8 * index + index2]) resultArray[index] += 2 ** (7 - index2)
+
+            //verify checksum
+            checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
+            checksum = resultArray[4]
+            if (checksumTmp >= 512) checksumTmp -= 512
+            if (checksumTmp >= 256) checksumTmp -= 256
+            if (checksum == checksumTmp) _readSuccessful = true
+
+            //read data if checksum ok
+            if (_readSuccessful) {
+
+
                 _humidity = resultArray[0] + resultArray[1] / 100
                 _temperature = resultArray[2] + resultArray[3] / 100
+
+
+
+
+
+
+
                 _last_successful_query_humidity = _humidity
-                _last_successful_query_temperature = _temperature         
-        }else{
-            _humidity = _last_successful_query_humidity
-            _temperature = _last_successful_query_temperature
+                _last_successful_query_temperature = _temperature
+            } else {
+                _humidity = _last_successful_query_humidity
+                _temperature = _last_successful_query_temperature
+            }
         }
+        //wait 1.5 sec after query 
+        basic.pause(1500)
     }
-		 //wait 1.5 sec after query 
-    basic.pause(1500)
-}
 
     //% block="DHT11 Read %dht11data| at pin %dht11pin|"
-	//% weight=150
+    //% weight=150
     export function readData(dht11data: DHT11dataType, dht11pin: DigitalPin): number {
-		// querydata
-		dht11_queryData(dht11pin)
-		//return temperature /humidity
-		// if(dht11data == DHT11dataType.temperature && _readSuccessful)
-			// return Math.round (_temperature)
-		// else if(dht11data == DHT11dataType.humidity && _readSuccessful)
-			// return Math.round (_humidity)
-		// else return 0
-		if (dht11data == DHT11dataType.temperature){
-            return Math.round(_temperature)}
-        else 
-            return Math.round(_humidity) 
+        // querydata
+        dht11_queryData(dht11pin)
+        //return temperature /humidity
+        // if(dht11data == DHT11dataType.temperature && _readSuccessful)
+        // return Math.round (_temperature)
+        // else if(dht11data == DHT11dataType.humidity && _readSuccessful)
+        // return Math.round (_humidity)
+        // else return 0
+        if (dht11data == DHT11dataType.temperature) {
+            return Math.round(_temperature)
+        }
+        else
+            return Math.round(_humidity)
     }
 
-	
-	//% blockId=read_distance_sensor
-	//% block="Get distance unit %unit trig %trig echo %echo"
-	//% weight=140
-	//% trig.defl=DigitalPin.P14 echo.defl=DigitalPin.P15
-	//% inlineInputMode=inline
+
+    //% blockId=read_distance_sensor
+    //% block="Get distance unit %unit trig %trig echo %echo"
+    //% weight=140
+    //% trig.defl=DigitalPin.P14 echo.defl=DigitalPin.P15
+    //% inlineInputMode=inline
     export function read_distance_sensor(unit: DistanceUnit, trig: DigitalPin, echo: DigitalPin, maxCmDistance = 500): number {
         // send pulse
-        let d=10;
+        let d = 10;
         pins.setPull(trig, PinPullMode.PullNone);
-        for (let x=0; x<10; x++)
-        {
+        for (let x = 0; x < 10; x++) {
             pins.digitalWritePin(trig, 0);
             control.waitMicros(2);
             pins.digitalWritePin(trig, 1);
@@ -236,25 +267,29 @@ namespace SmartCity {
             pins.digitalWritePin(trig, 0);
             // read pulse
             d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
-            if (d>0)
+            if (d > 0)
                 break;
         }
 
         switch (unit) {
-            case DistanceUnit.Centimeters: 
-                if(control.ramSize()<20000){
-                    return Math.round(d/58*1.4);}
-                else{
-                    return Math.round(d/58);}
-            case DistanceUnit.Inches: 
-                if(control.ramSize()<20000){
-                    return Math.round(d/148*1.4);}
-                else{
-                    return Math.round(d/148);}
-            default: return d ;
+            case DistanceUnit.Centimeters:
+                if (control.ramSize() < 20000) {
+                    return Math.round(d / 58 * 1.4);
+                }
+                else {
+                    return Math.round(d / 58);
+                }
+            case DistanceUnit.Inches:
+                if (control.ramSize() < 20000) {
+                    return Math.round(d / 148 * 1.4);
+                }
+                else {
+                    return Math.round(d / 148);
+                }
+            default: return d;
         }
     }
-	
+
 
 }
 
